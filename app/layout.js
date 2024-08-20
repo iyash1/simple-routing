0ab15aeacd97c7b8f418766a03c419f350057bab
Yash Eturi <yashwanthe@gmail.com>
2024-08-20 16:47:56 +0530

    FIX1: For CSP policy issue

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+          crossorigin="anonymous"
+        />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="
+                        connect-src 'self' https://cdn.jsdelivr.net;
+                        style-src 'self' 'unsafe-inline';
+                        img-src 'self' https://images.unsplash.com;
+                        font-src 'self';
+                        object-src 'none';
+                    "
         />
       </head>
       <body className={quickSand.className}>
